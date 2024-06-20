216856281fdf10d22ade4d2247082fc272fcc3bb
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-06-20 13:29:52 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+let age=19
+let name="yash"
+let loggedin = true
+let stdId=null
+// type of null in javascript is object
+
+console.log(typeof loggedin)
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof stdId)
+
