dfd6842910fd54f3a755825d4141c2fa5a2bdf6d
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-06-20 12:18:47 +0000

    container config done

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -1,1 +1,12 @@
-const a=5;
+const AccountId=123321
+let AccountEmail="yp121@google.com"
+var AccountPass="qwe321"
+let AccountState;
+
+
+console.log(AccountId)
+/* 
+prefer not to use var because of the problem in block scope and functional scope
+*/
+
+console.table([ AccountEmail, AccountId , AccountPass,AccountState])
