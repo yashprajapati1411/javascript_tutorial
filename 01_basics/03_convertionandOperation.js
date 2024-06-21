3d5fc1039ef98e6234af423cdd3871b42cf7f483
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-06-21 11:03:34 +0000

    convertion in javascript

---
+++ 01_basics/03_convertionandOperation.js
@@ -0,0 +1,32 @@
+let score="33abd"
+
+//console.log(typeof(score));
+
+let valueofScore=Number(score)
+
+//console.log(typeof(valueofScore))
+//console.log(valueofScore)
+
+//  "33"=>33 
+//  "33abc"=>NAN
+// "true"=>1 
+
+let isslogged = 1
+
+let boolissloggedin=Boolean(isslogged)
+//console.log(boolissloggedin)
+//console.log(typeof boolissloggedin)
+
+// 1=>true
+//""=>false
+//"yash"=>true
+
+let stringnum=33
+
+console.log(typeof stringnum)
+
+let converttoString=String(stringnum)
+console.log(converttoString)
+console.log(typeof(converttoString))
+
+
