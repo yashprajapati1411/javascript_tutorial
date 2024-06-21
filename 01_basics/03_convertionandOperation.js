4f0c16c5a49cf6d9631eb729498746d0fa042d74
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-06-21 12:04:37 +0000

    convertion of one data type to another

--- 01_basics/03_convertionandOperation.js
+++ 01_basics/03_convertionandOperation.js
@@ -1,32 +1,68 @@
-let score="33abd"
+let score = "hitesh"
 
+//console.log(typeof score);
 //console.log(typeof(score));
 
-let valueofScore=Number(score)
-
-//console.log(typeof(valueofScore))
-//console.log(valueofScore)
-
-//  "33"=>33 
-//  "33abc"=>NAN
-// "true"=>1 
-
-let isslogged = 1
-
-let boolissloggedin=Boolean(isslogged)
-//console.log(boolissloggedin)
-//console.log(typeof boolissloggedin)
-
-// 1=>true
-//""=>false
-//"yash"=>true
-
-let stringnum=33
-
-console.log(typeof stringnum)
-
-let converttoString=String(stringnum)
-console.log(converttoString)
-console.log(typeof(converttoString))
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+ console.log(2/3);
+ console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
