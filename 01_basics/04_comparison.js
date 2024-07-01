71f096b6df12fbc543f688f0cf8940433b8a117d
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-07-01 10:04:42 +0000

    comparison in javascript

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,25 @@
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+/*should avoid using below comparison */
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// ===  it check both the data type and value
+
+console.log("2" === 2); //output false
