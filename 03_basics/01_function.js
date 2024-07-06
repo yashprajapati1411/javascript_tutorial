c54c95e883787520f03a5fb133d851750654293f
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-07-06 05:49:06 +0000

    function in javascript

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -67,4 +67,4 @@ function returnSecondValue(getArray){
 }
 
 // console.log(returnSecondValue(myNewArray));
-console.log(returnSecondValue([200, 400, 500, 1000]));
+console.log(returnSecondValue([200,400, 500, 1000]));
