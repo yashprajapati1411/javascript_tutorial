d6a0acd120d9acaec3ae1abe351d34f255309f0a
Yash Prajapati <138312580+yashprajapati1411@users.noreply.github.com>
2024-07-17 11:55:43 +0000

    do while loop

---
+++ 05_Iteration/two.js
@@ -0,0 +1,25 @@
+// while 
+let index =0
+while (index <=10) {
+
+    //console.log(`value of index is : ${index}`)
+    index++;
+
+}
+
+let myArr=["superman","batman","flash"]
+
+let arr=0
+while(arr<myArr.length){
+    //
+    console.log(myArr[arr])
+    arr++;
+}
+
+// do while
+let score=1
+do {
+    console.log(`score is ${score}`)
+    score++;
+    
+} while (score<10);
